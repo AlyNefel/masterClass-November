d2674caa3d8225727b3d1ec4b16d0344e28efd2b
alynefel <ali.nefel2020@gmail.com>
2025-12-14 10:57:30 +0000

    .map, .filter .find

--- javascript/ES6/script.js
+++ javascript/ES6/script.js
@@ -109,7 +109,7 @@
 //  console.log(message2)
  // const result = test>var?"value of result if true":"value result if false"
 
- //4-Destructuring : methode bech na3mlou etraction of values from array or object 
+ //4-Destructuring : methode bech na3mlou extraction of values from array or object 
  // before ES6  : var person ={name:"Person",age:28} 
  // var name = person.name // "Person"
  const person ={
@@ -163,3 +163,122 @@ let array3=[array1,...array2]
 const addSpread=(x,y,z)=>x+y+z
 
 console.log(addSpread(...numbers))
+
+// [{},{},{},{}]
+// intro to getter & setter 
+// [true,5,"test",function]
+let state="value"
+const useState=[
+   // index 0 will hold a function
+   function getState(){
+      console.log(state)
+return state
+   },
+   // index 1 will hold another function
+   function setState(newValue){
+      // eg state = "newValue"
+      state =newValue
+      // console.log(`new value of state variable after re-assign : ${state}`)
+      return state
+   }
+]
+const [getter,setter]=useState  // getter =useState[0] =>getState function
+console.log(getter)
+// console.log(useState[0],useState[1])
+// // get the state variable 
+// useState[0]() // getState()
+// //set the state value (chnage it)
+// useState[1]("Karima") // setState("Karima")
+// // get the state one more time 
+// useState[0]()
+
+
+let colors =["red","blue","green"]
+const useColors=[
+   function getter(){
+      console.log(colors)
+      return colors
+   },
+   function setter(newColor){
+      //spread operator
+    colors=[...colors,newColor]
+    return colors
+   }
+]
+const [getColors,setColors]=useColors
+
+// getter => useColors[0]
+getColors()
+// add new color to the array using useColors[1]
+setColors("white")
+// get the colors after update (set)
+getColors()
+
+
+
+//6-call back function => use a function inside another function
+let fruits=["Banana","APPle","cherry"] 
+//=>
+  // 
+// step1 eg : function that takes a word as param and return it in uppercase
+const capitalWord=(word)=>{
+   return word.toUpperCase()
+}
+let test="HGHhkjjhJKHJjhjh"
+console.log(capitalWord(test))
+
+// step 2 main function 
+const processFruits=(array,callback)=>{
+for (let index = 0; index < array.length; index++) {
+   // kol element mel array bech na3ml 3lihg application ta3 callback function
+   array[index]=callback(array[index])
+   
+}
+console.log(array)
+return array
+}
+
+processFruits(fruits,capitalWord)
+
+
+//2nd eg 
+const mainFunction=(array,callback)=>{
+   const result=[]
+   // for loop 
+   for (let index = 0; index < array.length; index++) {
+     result.push(callback(array[index]))      
+   }
+   console.log(result)
+}
+
+// square function 
+const squareFunction=(number)=>{
+   return number*number
+}
+// function that add +2 
+const plus2=(number)=>{
+   return number+=2
+}
+
+mainFunction([2,4,5,8],squareFunction)
+mainFunction([2,4,5,8],plus2)
+
+// .map : creates new array  // ta3ml apply ta3 call back function 3la kol element fil array 
+
+let numArray=[2,4,5,-1,12,77]
+// mainFunction(numArray,suqareFunction)
+numArray.map(num=>num*num)
+console.log(numArray.map(num=>num*num)
+)
+console.log(numArray)
+console.log(numArray.map(numberrr=>numberrr+=10))
+
+//.filter == ta3mel creation ta3 array jdid yraja3 les element li verifou condition mou3ayna (callback)
+const numberGreaterThen3=numArray.filter(num=>num>3)
+const evenNumbers=numArray.filter(num=>num%2===0)
+console.log(numberGreaterThen3)
+console.log(evenNumbers)
+
+// .find : similar to filter but return the first element that satisfies the condition 
+const firstEven=numArray.find(num=>num%2===0)
+console.log(firstEven)
